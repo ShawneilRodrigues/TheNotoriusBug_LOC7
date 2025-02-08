cc23d4e700b7157627ea66fe9a11c94aa5c157e0
Taarak Gada <taarakgada4151@gmail.com>
2025-02-08 20:32:02 +0530

    feat: add smooth scrolling behavior and enhance Navbar links for better navigation

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -16,22 +16,37 @@ export default function Navbar() {
 
     return (
         <nav className="mx-20 px-4 py-2 rounded-md flex items-center justify-between">
-          <div className="text-3xl font-bold text-gray-800">AISupportAgent</div>
-          <div className="w-1/3 p-3 rounded-sm flex justify-between items-center gap-5">
-            <Link href="/" className="text-gray-800 hover:text-gray-800">
-              Home
-            </Link>
-            <Link href="#features" className="text-gray-800 hover:text-gray-800">
-              Features
-            </Link>
-            <Link href="#contact" className="text-gray-800 hover:text-gray-800">
-              Contact Us
-            </Link>
-            <Link href="/login" className="text-gray-800 hover:text-gray-800">
-              Login
-            </Link>
-            <ModeToggle />
-          </div>
+            <div className="text-3xl font-bold text-gray-800">
+                AISupportAgent
+            </div>
+            <div className="w-1/3 p-3 rounded-sm flex justify-between items-center gap-5">
+                <Link
+                    href="/"
+                    className="text-gray-800 hover:text-gray-800"
+                >
+                    Home
+                </Link>
+                <Link
+                    scroll
+                    href="#features"
+                    className="text-gray-800 hover:text-gray-800"
+                >
+                    Features
+                </Link>
+                <Link
+                    href="#contact"
+                    className="text-gray-800 hover:text-gray-800"
+                >
+                    Contact Us
+                </Link>
+                <Link
+                    href="/login"
+                    className="text-gray-800 hover:text-gray-800"
+                >
+                    Login
+                </Link>
+                <ModeToggle />
+            </div>
         </nav>
     );
 }
